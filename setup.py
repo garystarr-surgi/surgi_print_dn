f3580b2dd1122572bc62e3caec93e6dbdb15045b
garystarr-surgi <gary.starr@surgishop.com>
2025-10-07 09:46:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-if __name__ == '__main__':
-    setup()
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
+setup(
+    name="surgi_print",
+    version="0.0.1",  # <-- make sure this matches __init__.py
+    description="Custom printing integration for ERPNext",
+    author="Your Name",
+    author_email="your@email.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
+)
