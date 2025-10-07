978fde61fa8b1f42e68470c8607e3105a182e281
garystarr-surgi <gary.starr@surgishop.com>
2025-10-07 09:43:51 -0400

    Update setup.py

--- surgi_print/setup.py
+++ surgi_print/setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
 setup(
     name='surgi_print',
     version='0.0.5',
@@ -8,4 +11,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True
+    install_requires=install_requires,
 )
