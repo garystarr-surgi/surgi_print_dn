d9194a90d5c9d6c50abc95168d77f06efe40e230
garystarr-surgi <gary.starr@surgishop.com>
2025-10-07 09:46:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("requirements.txt") as f:
 
 setup(
     name="surgi_print",
-    version="0.0.1",  # <-- make sure this matches __init__.py
-    description="Custom printing integration for ERPNext",
-    author="Your Name",
-    author_email="your@email.com",
+    version="0.0.5",  # <-- make sure this matches __init__.py
+    description="CUPS printing for Delivery Note",
+    author="Gary Starr",
+    author_email="gary.starr@surgishop.com",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
