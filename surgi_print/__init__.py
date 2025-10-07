5707867602c4eee5ea8c851196cec222d835a927
garystarr-surgi <gary.starr@surgishop.com>
2025-10-07 11:19:02 -0400

    Update __init__.py

--- surgi_print/__init__.py
+++ surgi_print/__init__.py
@@ -1,8 +1,4 @@
 # surgi_print/__init__.py
 
-from __future__ import unicode_literals
+from ._version import __version__
 
-# This line is crucial for bench to find the version.
-__version__ = "0.0.5" 
-
-# Ensure this file is clean of external code like 'setup()'
