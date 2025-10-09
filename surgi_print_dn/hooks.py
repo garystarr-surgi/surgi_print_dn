8ec515825b69fd59ccea5ab1e73b14aba9a01ad4
garystarr-surgi <gary.starr@surgishop.com>
2025-10-09 12:06:29 -0400

    Update and rename hooks.py to hooks.py

--- surgi_print_dn/hooks.py
+++ surgi_print_dn/hooks.py
@@ -1,5 +1,5 @@
-app_name = "surgi_print"
-app_title = "Surgi Print"
+app_name = "surgi_print_dn"
+app_title = "Surgi Print DN"
 app_publisher = "Gary Starr"
 app_description = "CUPS Printing Integration for Delivery Notes"
 app_version = "0.0.5"
@@ -7,7 +7,7 @@ app_license = "MIT"
 
 # Load JS only for Delivery Note
 doctype_js = {
-    "Delivery Note": "public/js/delivery_note_print.js"
+    "Delivery Note": "public/js/dn_print.js"
 }
 
 # Optional: list Python dependencies
