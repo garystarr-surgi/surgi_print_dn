63a2a2750aa00a7e5b469d3550432286827e8539
garystarr-surgi <gary.starr@surgishop.com>
2025-10-09 11:43:56 -0400

    Update hooks.py

--- surgi_print_dn/hooks.py
+++ surgi_print_dn/hooks.py
@@ -1,5 +1,5 @@
 app_name = "surgi_print_dn"
-app_title = "Surgi_Print_DN"
+app_title = "Surgi Print DN"
 app_publisher = "Gary Starr"
 app_description = "CUPS Printing Integration for Delivery Notes"
 app_version = "0.0.5"
