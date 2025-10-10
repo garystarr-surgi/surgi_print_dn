d24c930fd192d8ee854b8d72b9905d2c7fab5b15
garystarr-surgi <gary.starr@surgishop.com>
2025-10-10 09:18:59 -0400

    Update __init__.py

--- surgi_print_dn/__init__.py
+++ surgi_print_dn/__init__.py
@@ -1,15 +1,1 @@
-# The technical name of your app (must match the folder name)
-app_name = "surgi_print_dn"
-
-# The display title shown in the Frappe UI (e.g., in "About" or the sidebar)
-__title__ = "Surgi Print DN"
-
-# The current version of your application
-__version__ = "0.0.5" 
-
-# Metadata for the app (can be expanded if needed)
-__app_name__ = "surgi_print_dn"
-__app_title__ = "Surgi Print DN"
-__description__ = "Custom application for specialized printing functionality."
-__license__ = "MIT"
-__module_name__ = "Surgi Print DN"
+from ._version import __version__
